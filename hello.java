308f0c2c5eff8772d856d70defcffcd52ed1c3f9
nareshr <nareshr@gmail.com>
2017-02-23 18:18:11 +0530

    hellojava

---
+++ hello.java
@@ -0,0 +1,14 @@
+ Public class Hello{
+
+   Public Static void main(string[] args){
+    
+     for (int i=1; i<=10; i++)
+   
+      Sysytem.out.printIn("Hello world.." +i);
+         
+              }
+      
+          }
+     
+       } 
+
